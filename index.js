29d42a2ffbdacb7a827b67f8cc50ef2f80cce201
barathga <barathga1999@gmail.com>
2022-02-27 18:40:20 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongo = require('./shared/connect');
+const {Studentsignin,signup} = require('./modules/registermodule');
+
+const authorizeModule = require('./modules/authorize');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+mongo.connect();
+
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+app.post('/signin',Studentsignin);
+app.post('/signup',signup); 
+app.use(authorizeModule.AuthenticateUser);
+
+
+
+app.listen(process.env.PORT||3001);
